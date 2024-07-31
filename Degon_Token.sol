1d7ac63de097dd278e348ef536c8fd41b642ad9f
PallaviSingh262215036 <154880018+PallaviSingh262215036@users.noreply.github.com>
2024-07-31 14:34:13 +0530

    Update Degon_Token.sol

--- Degon_Token.sol
+++ Degon_Token.sol
@@ -10,8 +10,6 @@ contract DegenToken is ERC20 {
     uint256 public rewardPercentage = 2; // 2% reward for holding tokens
     uint256 public rewardPeriod = 30 days; // Reward period
     address public owner;
-    uint256 public TotalSupply;
-
 
     // Structure to represent an in-game item
     struct Item {
@@ -23,7 +21,6 @@ contract DegenToken is ERC20 {
     Item[] public items;
 
 
-    mapping(address=>uint256) public BalanceOf; 
     // Mapping to track redeemed items by user
     mapping(address => mapping(uint256 => uint256)) public redeemedItems;
     mapping(address => uint256) public lastClaimedReward;
@@ -48,54 +45,43 @@ contract DegenToken is ERC20 {
     // Minting new tokens with time lock
     
     function mintToken(address to, uint256 amount) public onlyOwner {
-        require(to!=address(0),"You are minting to Zero address");
-         BalanceOf[to]+=amount;
-         TotalSupply+=amount;   
+        _mint(to, amount); 
     }
 
-    // Redeeming tokens for items
+      // Redeeming tokens for items
     function redeem(uint256 itemId, uint256 quantity) external {
         require(itemId < items.length, "Invalid item ID");
         require(quantity > 0, "Quantity must be greater than 0");
 
         uint256 totalCost = items[itemId].cost * quantity;
-        require(BalanceOf[msg.sender] >= totalCost, "Not enough tokens to redeem");
+        require(balanceOf(msg.sender) >= totalCost, "Not enough tokens to redeem");
 
-        BalanceOf[msg.sender]-=totalCost;
-        redeemedItems[msg.sender][itemId] += quantity;  
-    }
+        _burn(msg.sender, totalCost);
+        redeemedItems[msg.sender][itemId] += quantity;
 
-    // Burning tokens
-    function burnToken(uint256 amount) public {
-        require(BalanceOf[msg.sender] >= amount, "Not enough tokens to burn");
-        BalanceOf[msg.sender]-=amount;
-        TotalSupply-=amount;
         
     }
-
-   //Transfer logic internaal function
-   function transfer_logic(address from, address to, uint256 amount) internal {
-     require(from!=address(0),"Invalid address");
-     require(to!=address(0),"You are sending to invalid address");
-     require(BalanceOf[from]>amount,"Insufficient balance");
-     BalanceOf[from]-=amount;
-     BalanceOf[to]+=amount;
-   }
+    // Burning tokens
+    function burnToken(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
+        _burn(msg.sender, amount);
+        
+    }
    
     //  Transfer function to include fee and reward mechanism
     function transfer( address recipient, uint256 amount) public override returns(bool) {
         uint256 fee = (amount * transferFeePercentage) / 100;
         uint256 transferAmount = amount - fee;
-
+        
         // Transfer fee to treasury
-        transfer_logic(msg.sender, treasury, fee);
+        _transfer(msg.sender, treasury, fee);
 
         // Transfer remaining tokens to recipient
-        transfer_logic(msg.sender, recipient, transferAmount);
+        _transfer(msg.sender, recipient, transferAmount);
 
         // Update last claimed reward timestamp
         if (block.timestamp >= lastClaimedReward[msg.sender] + rewardPeriod) {
-            uint256 reward = (BalanceOf[msg.sender] * rewardPercentage) / 100;
+            uint256 reward = (balanceOf(msg.sender) * rewardPercentage) / 100;
             mintToken(msg.sender, reward);
             lastClaimedReward[msg.sender] = block.timestamp;
         }
